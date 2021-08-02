666dece78b1a457f2175ab600e98e3c85ea5c5d3
Jnki3Mav3n <manu.ar217@gmail.com>
2021-08-01 22:58:02 -0600

    Gave Component Functionalities to Add Contacts

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/App.css';
 import Header from './Header';
 import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
-  const contacts = [
-    {
-      id: "001",
-      name: "Manuel",
-      email: "manu.ar217@gmail.com"
-    },
-    {
-      id: "002",
-      name: "Nelly",
-      email: "nategaleano@gmail.com"
-    }
-  ];
+  const [contacts, setContacts] = useState([]);
+
+  // {
+  //   id: "001",
+  //   name: "Manuel",
+  //   email: "manu.ar217@gmail.com"
+  // },
+  // {
+  //   id: "002",
+  //   name: "Nelly",
+  //   email: "nategaleano@gmail.com"
+  // }
+
+  const addContactHandler = contact => {
+    console.log(contact);
+  };
 
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
+      <AddContact addContactHandler={addContactHandler}/>
       <ContactList contacts={contacts}/>
     </div>
   );
