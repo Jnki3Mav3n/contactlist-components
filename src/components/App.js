2a60e4fb651024669fd8208c3a0a17fedc420458
Jnki3Mav3n <manu.ar217@gmail.com>
2021-08-15 23:59:21 -0600

    Configured Routed Components Being Switched

--- src/components/App.js
+++ src/components/App.js
@@ -7,55 +7,55 @@ import AddContact from './AddContact';
 import ContactList from './ContactList';
 
 function App() {
-  const localStorageKey = "contacts";
-  const [contacts, setContacts] = useState([]);
+    const localStorageKey = "contacts";
+    const [contacts, setContacts] = useState([]);
 
-  // {
-  //   id: "001",
-  //   name: "Manuel",
-  //   email: "manu.ar217@gmail.com"
-  // },
-  // {
-  //   id: "002",
-  //   name: "Nelly",
-  //   email: "nategaleano@gmail.com"
-  // }
+    // {
+    //   id: "001",
+    //   name: "Manuel",
+    //   email: "manu.ar217@gmail.com"
+    // },
+    // {
+    //   id: "002",
+    //   name: "Nelly",
+    //   email: "nategaleano@gmail.com"
+    // }
 
-  const addContactHandler = contact => {
-    console.log(contact);
-    setContacts([...contacts, {id: uuid(), ...contact}]);
-  };
+    const addContactHandler = contact => {
+        console.log(contact);
+        setContacts([...contacts, {id: uuid(), ...contact}]);
+    };
 
-  const remContactHandler = id => {
-    const newCList = contacts.filter(contact => {
-      return contact.id !== id;
-    });
+    const remContactHandler = id => {
+        const newCList = contacts.filter(contact => {
+            return contact.id !== id;
+        });
 
-    setContacts(newCList);
-  }
+        setContacts(newCList);
+    };
 
-  useEffect(() => {
-    const getContacts = JSON.parse(localStorage.getItem(localStorageKey));
-    if (getContacts) setContacts(getContacts);
-  }, []);
+    useEffect(() => {
+        const getContacts = JSON.parse(localStorage.getItem(localStorageKey));
+        if (getContacts) setContacts(getContacts);
+    }, []);
 
-  useEffect(() => {
-    localStorage.setItem(localStorageKey, JSON.stringify(contacts));
-  }, [contacts]);
+    useEffect(() => {
+        localStorage.setItem(localStorageKey, JSON.stringify(contacts));
+    }, [contacts]);
 
-  return (
-    <div className="ui container">
-      <Router>
-        <Header />
-        <Switch>
-          <Route path="/add" component={AddContact} />
-          <Route path="/" component={ContactList} />
-        </Switch>
-        {/* <AddContact addContactHandler={addContactHandler}/>
-        <ContactList contacts={contacts} getContactId={remContactHandler}/> */}
-      </Router>
-    </div>
-  );
-}
+    return (
+        <div className="ui container">
+            <Router>
+                <Header />
+                <Switch>
+                    <Route path="/" component={() => <ContactList contacts={contacts} getContactId={remContactHandler}/>} exact />
+                    <Route path="/add" component={() => (<AddContact addContactHandler={addContactHandler} />)} />                    
+                </Switch>
+                {/* <AddContact addContactHandler={addContactHandler}/>
+                <ContactList contacts={contacts} getContactId={remContactHandler}/> */}
+            </Router>
+        </div>
+    );
+};
 
 export default App;
--- src/components/ContactList.js
+++ src/components/ContactList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import ContactCard from './ContactCard';
 
 const ContactList = props => {
@@ -8,27 +9,36 @@ const ContactList = props => {
         props.getContactId(id);
     };
 
-    const contacts = [
-                {
-            id: "001",
-            name: "Manuel",
-            email: "manu.ar217@gmail.com"
-        },
-        {
-            id: "002",
-            name: "Nelly",
-            email: "nategaleano@gmail.com"
-        }
-    ]
-    const renderCList = contacts.map(contact => {
+    // const contacts = [
+    //     {
+    //         id: "001",
+    //         name: "Manuel",
+    //         email: "manu.ar217@gmail.com"
+    //     },
+    //     {
+    //         id: "002",
+    //         name: "Nelly",
+    //         email: "nategaleano@gmail.com"
+    //     }
+    // ];
+
+    const renderCList = props.contacts.map(contact => {
         return <ContactCard contact={contact} clickHandler={delContact} key={contact.id}></ContactCard>;
-    })
+    });
 
     return (
-        <div className="ui celled list">
-            {renderCList}
+        <div class="main">
+            <h2>
+                Contact List
+                <Link to="/add">
+                    <button className="ui button blue right">Add Contact</button>
+                </Link>
+            </h2>
+            <div className="ui celled list">
+                {renderCList}
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default ContactList
